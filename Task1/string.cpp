305adb83405a5692aca8af99af364a6bc240d6ff
Portfilum01 <sabastiandust@gmail.com>
2025-02-10 18:45:19 +1030

    Added Notes
    Added notes to two functions in string.cpp at Line 5 and Line 11

--- Task1/string.cpp
+++ Task1/string.cpp
@@ -2,12 +2,13 @@
 #include "String.h"
 using namespace std;
 
-
+// Creates an empty string
 String::String()
 {
 	string string = "";
 }
 
+// Creates a string using a pointer to a C String.
 String::String(const char* _str)
 {
 	const char cString{};

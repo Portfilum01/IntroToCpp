837039d1ac655c88874f9c4f33fa1f74dbe735bb
Portfilum01 <sabastiandust@gmail.com>
2025-02-13 16:10:50 +1030

    Updated empty string struct & string from string struct
    Fixed the main problem with my empty string.
    
    Made a start on creating a string from a cstring with a pointer.
    
    Also, there's a commented out test section inside of string.cpp (testing it reveals that the program does not work)

--- Task1/string.cpp
+++ Task1/string.cpp
@@ -6,7 +6,7 @@ using namespace std;
 String::String()
 {
 	//takes my string and makes an array with only 1 value
-	char* myString = new char[1];
+	myString = new char[1];
 
 	//Makes it into an empty string by assigning 0 to the 0th position.
 	myString[0] = 0;
@@ -14,6 +14,18 @@ String::String()
 
 String::String(const char* _str)
 {
-	
+	// Gets the length of the string
+	int lengthOfString = strlen(_str);
+
+	//Changes the size of the char array in myString
+	char* myString = new char[lengthOfString + 1];
+
+	// Copies the _str to myString
+	strcpy_s(myString, lengthOfString, _str);
 }
 
+//int main()
+//{
+//	String::String("mantis");
+//	cout << *myString;
+//}

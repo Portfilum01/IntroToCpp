96447129ea27e1881151b1824eebbceef5b29a52
Portfilum01 <sabastiandust@gmail.com>
2025-02-13 11:13:01 +1030

    Added a note
    Added a clarifying note to the empty string constructor.

--- Task1/string.cpp
+++ Task1/string.cpp
@@ -7,6 +7,8 @@ String::String()
 {
 	//takes my string and makes an array with only 1 value
 	char* myString = new char[1];
+
+	//Makes it into an empty string by assigning 0 to the 0th position.
 	myString[0] = 0;
 }
 

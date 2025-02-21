34bb642de61190a9b52443dc8e8b6b862dbd8a36
Portfilum01 <sabastiandust@gmail.com>
2025-02-21 14:24:17 +1030

    Added new variables + Percentage calculator.
    Added a new variable, and will continue this formula in order to print whether or not something has succeeded (unless I find a better method).
    
    Also, down towards the bottom where it adds the test results to the file, it calculates the percentage of successful tests.

--- Task1/Source.cpp
+++ Task1/Source.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include "String.h"
+#include <fstream>
+#include <ctime>
+#include <chrono>
+
 using namespace std;
+
 
 int main()
 {
+	// Making a test file
+	std::fstream testFile;
+	testFile.open("Test File.txt", ios::out);
+	int percentageCalculator = 0;
+	string stringConstructor1 = "Test 0: Constructor Failed";
+
 	// empty string
 	String();
 
 	// String constructors (a few for use in later functions
 	String Object1("Hello World");
+	if (Object1 == "Hello World")
+		percentageCalculator += 1;
+		stringConstructor1 = "Test 0: Constructor Successful";
 
 	String Object2(" Hi!");
 
-	const String ConstantObject("Blam");
+	const String ConstantObject("Test");
 
 	//String constructed from pointing to another string
 	String CopyObject(Object1);
@@ -51,5 +65,9 @@ int main()
 
 	// Less than
 	Object1 < Object2;
+
+	percentageCalculator = (percentageCalculator / 17) * 100;
+	testFile << "Date: " << __DATE__ << " Time: " << __TIME__ << " " << percentageCalculator << "% of tests succeeded" << '\n' << endl;
+	testFile << stringConstructor1 << endl;
 }
 

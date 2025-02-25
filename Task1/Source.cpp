e6f3503a21a1542cf9bd4f0002fe0c788c65a4e5
Portfilum01 <sabastiandust@gmail.com>
2025-02-26 09:10:18 +1030

    Added Text File Framework
    Added the framework for the program to write test results to a text file.

--- Task1/Source.cpp
+++ Task1/Source.cpp
@@ -9,59 +9,26 @@ using namespace std;
 
 int main()
 {
+	const int testsToRun = 0;
+
+	const char* testNames[testsToRun]   = {};
+	bool        testResults[testsToRun] = {};
+
+
+
+	for (int test = 0; test < testsToRun; ++test)
+	{
+		std::cout
+			<< "Test " << test << " " << testNames[test]
+			<< " "
+			<< (testResults[test] ? "Successful" : "Failed")
+			<< std::endl;
+	}
+
 	// Making a test file
 	std::fstream testFile;
 	testFile.open("Test File.txt", ios::out);
 	int percentageCalculator = 0;
 
-	// empty string
-	String();
-
-	// String constructors (a few for use in later functions
-	String Object1("Hello World");
-
-	String Object2(" Hi!");
-	const String ConstantObject("Test");
-
-	//String constructed from pointing to another string
-	String CopyObject(Object1);
-
-	// Length
-	Object1.Length();
-
-	// Append
-	Object1.Append(Object2);
-
-	// Convert all to lowercase
-	Object1.ToLower();
-
-	// Convert all to uppercase
-	Object1.ToUpper();
-
-	// Find a character
-	Object1.FindCharacter('O');
-	
-	// Replace a character
-	Object1.Replace('L', 'U');
-
-	// Read from and Write to console
-	Object1.ReadFromConsole();
-	CopyObject.WriteToConsole();
-
-	// If equal to
-	Object1 == Object2;
-	
-	// Subscript Operators
-	Object1[2];
-	ConstantObject[3];
-
-	// Becomes equal to
-	Object1 = Object2;
-
-	// Less than
-	Object1 < Object2;
-
-	percentageCalculator = (percentageCalculator / 17) * 100;
-	testFile << "Date: " << __DATE__ << " Time: " << __TIME__ << " " << percentageCalculator << "% of tests succeeded" << '\n' << endl;
 }
 
